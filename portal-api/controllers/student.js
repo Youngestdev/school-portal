956e8be8f75fb05155d340dd155a5d72a74a7a9e
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-01-13 14:10:55 +0100

    :cry:

--- portal-api/controllers/student.js
+++ portal-api/controllers/student.js
@@ -32,22 +32,23 @@ exports.create = (req, res) => {
     });
 };
 
-exports.update = (req, res) => {
-  StudentModel.updateOne({_id: req.params.id}, {
-    $set:
-      {
-        title: req.body.title,
-        address: req.body.address,
-        neighborhood: req.body.neighborhood,
-        date: req.body.date
-      }
-  }).then(student => {
-    res.json("Item updated successfully");
-  })
-    .catch(err => {
-      res.status(400).send("Unable to update item.")
-    });
-};
+// My head is too hot. Let's someone help sef :grin:
+// exports.update = (req, res) => {
+//   StudentModel.updateOne({_id: req.params.id}, {
+//     $set:
+//       {
+//         title: req.body.title,
+//         address: req.body.address,
+//         neighborhood: req.body.neighborhood,
+//         date: req.body.date
+//       }
+//   }).then(student => {
+//     res.json("Item updated successfully");
+//   })
+//     .catch(err => {
+//       res.status(400).send("Unable to update item.")
+//     });
+// };
 
 exports.delete = (req, res) => {
   StudentModel.findByIdAndRemove({_id: req.params.id}).then(student => {
